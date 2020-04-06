cf6c0f328d5c2e40bfc2bfa2d2496b866ea3d671
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-04-06 16:31:12 +0000

    Testing test.js

---
+++ test/tailTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+assertEqual(result.length, 2);
+assertEqual(result.join(), ["Lighthouse", "Labs"].join());
+
+assertEqual(result[0], "Lighthouse"); 
+assertEqual(result[1], "Labs"); 
+
