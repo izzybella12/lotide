2ff1b1d48350a32e585d055d15ba979b2080541d
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-04-06 18:13:09 +0000

    Added mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,21 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-
-assertEqual(result.length, 2);
-assertEqual(result.join(), ["Lighthouse", "Labs"].join());
-
-assertEqual(result[0], "Lighthouse"); 
-assertEqual(result[1], "Labs"); 
+describe("#tail", () => {
+  const result = tail(["Hello", "Lighthouse", "Labs"]);
+  
+  it('should return "2" when inputted "result.length', () => {
+    assert.deepEqual(result.length, 2); 
+  });
+  it('should return ["Lighthouse", "Labs"].join() when inputted result.join()', () => {
+    assert.deepEqual(result.join(), ["Lighthouse", "Labs"].join()); 
+  });
+  it('should return "Lighthouse" when inputted result[0]', () => {
+    assert.deepEqual(result[0], "Lighthouse");
+  });
+  it('should return "Labs" when inputted result[1]', () => {
+    assert.deepEqual(result[1], "Labs");
+  });
+});
 
