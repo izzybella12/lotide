79f158b473a38b756b8ce9383d50da17a6bf1140
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-04-06 20:12:49 +0000

    Added to index

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,5 @@
+const assertArraysEqual = require('./assertArraysEqual');
+
 const letterPositions = function(sentence) { //sentence is inputed
   const letterCount = {};
   for (let letter in sentence) {
@@ -10,35 +12,11 @@ const letterPositions = function(sentence) { //sentence is inputed
   return letterCount;
 };
 
-console.log(letterPositions("bella"));
+module.exports = letterPositions;
 
 //I've declared an object to push all our letters into;
 //when the sentence is entered, we have to divy up the letters in results
 //For this I've created a for...loop
 
-//
-
-const assertArraysEqual = function(firstArray, secondArray) {
-  let orderedFirstArray = "";
-  let orderedSecondArray = "";
-  for (let i = 0; i < firstArray.length - 1; i++) {
-    orderedFirstArray += firstArray[i] + ", ";
-  }
-  if (i = firstArray.length - 1) {
-    orderedFirstArray += firstArray[i];
-  }
-  for (let j = 0; j < secondArray.length - 1; j++) {
-    orderedSecondArray += secondArray[j] + ", ";
-  }
-  if (j = secondArray.length - 1) {
-    orderedSecondArray += secondArray[j];
-  }
-  if (orderedFirstArray === orderedSecondArray) {
-    console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${firstArrayy} !== ${secondArray}`);
-  }
-};
-
-assertArraysEqual(letterPositions("bella").b, 0);
+// console.log(assertArraysEqual(letterPositions("bella").b, [0]));
 
